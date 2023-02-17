5f83f02b9697b2ff9ce0186dcd10ef89b55893f6
sneg <sneg.sbg@gmail.com>
2023-02-17 13:47:59 +0300

    working

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string PrintNumbers(int n)
+{
+    if(n==1) return n.ToString();
+    else
+    {
+        return n + PrintNumbers(n -1);
+    }
+}
+
+void Main()
+{
+    Console.Write("Введите чсло N: ");
+    int n = int.Parse(Console.ReadLine()!);
+    Console.WriteLine($" N = {n} -> {PrintNumbers(n)}");
+}
+
+Main();
